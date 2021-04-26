1297f9b345894e6f5573b02c931b31085067c0da
Javan <71285290+djavanw@users.noreply.github.com>
2021-04-26 00:56:09 -0600

    Table data was correctly loaded. Department, Job, and then employee - in the order due to constraints.

--- data/dep_seeds.sql
+++ data/dep_seeds.sql
@@ -4,6 +4,6 @@ INSERT INTO department (d_id, dname) VALUES
 (10,"Sales"),
 (15,"Marketing"),
 (20,"Entertainment"),
-(25,"HR"),
+(25,"Accounting"),
 (30,"Logistics"),
 (35,"Administration");
--- data/emp_seeds.sql
+++ data/emp_seeds.sql
@@ -1,20 +1,24 @@
---This will allow the traditional insert into MySQL
---INSERT data into the employee table
+/*This will allow the traditional insert into MySQL
+INSERT data into the employee table*/
 USE emptracker_db;
 
-INSERT INTO employee (e_id, fname, lname, role_id, manager_id) VALUES 
-(050,"Kevin","Saurer",30,NULL),
-(060,"Jeff","Saurer",20,150),
-(070,"Morten","Harket",25,170),
-(080,"Sapphire","Howell",15,130),
-(090,"Donald","Cornelius",10,NULL),
-(100,"Jai","Wolf",30,050),
-(110,"Harrison","Mills",10,090),
-(120,"Clayton","Knight",15,130),
-(130,"Claude","VonStroke",15,NULL),
-(140,"Ellie","Goulding",10,090),
-(150,"Emilia","Clarke",20,NULL),
-(160,"Chandler","Riggs",35,150),
-(170,"Jeffery","Morgan",25,NULL),
-(180,"Aviram","Saharai",30,050),
-(190,"Isabelle","Rezazadeh",25,170);
+/*Managers*/
+INSERT INTO employee (e_id, fname, lname, role_id) VALUES 
+(050,"Kevin","Saurer",300),
+(090,"Donald","Cornelius",100),
+(130,"Claude","VonStroke",150),
+(150,"Emilia","Clarke",200),
+(170,"Jeffery","Morgan",250);
+
+/*Non Managers*/
+INSERT INTO employee (e_id, fname, lname, role_id, manager_id) VALUES
+(060,"Jeff","Saurer",200,150),
+(070,"Morten","Harket",250,170),
+(080,"Sapphire","Howell",150,130),
+(100,"Jai","Wolf",300,050),
+(110,"Harrison","Mills",100,090),
+(120,"Clayton","Knight",150,130),
+(140,"Ellie","Goulding",100,090),
+(160,"Chandler","Riggs",350,150),
+(180,"Aviram","Saharai",300,050),
+(190,"Isabelle","Rezazadeh",250,170);
