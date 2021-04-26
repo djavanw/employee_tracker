8b1f5320b436cdfb9e77e14cb33f5d2f22bbe77b
Javan <71285290+djavanw@users.noreply.github.com>
2021-04-25 19:58:38 -0600

    Tables loaded correctly in the Workbench.

--- schema.sql
+++ schema.sql
@@ -4,18 +4,6 @@ CREATE DATABASE emptracker_db;
 
 USE emptracker_db;
 
-DROP TABLE IF EXISTS employee;
-
-CREATE TABLE employee (
-    e_id INT PRIMARY KEY,
-    fname VARCHAR(30) NOT NULL,
-    lname VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NULL,
-    FOREIGN KEY (role_id) REFERENCES job(j_id),
-    FOREIGN KEY (manager_id) REFERENCES employee(e_id)
-);
-
 DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
@@ -32,3 +20,22 @@ CREATE TABLE job (
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(d_id)
 );
+
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee (
+    e_id INT PRIMARY KEY,
+    fname VARCHAR(30) NOT NULL,
+    lname VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES job(j_id),
+    FOREIGN KEY (manager_id) REFERENCES employee(e_id)
+);
+
+
+
+
+
+
+
