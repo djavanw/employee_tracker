f921bfc65730977b003421e2678b783b945296f2
Javan <71285290+djavanw@users.noreply.github.com>
2021-05-01 14:42:39 -0600

    Installed console.table for formatting.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-
+const consTable = require("console.table");
 
 const dataConnection = mysql.createConnection({
     host: "localhost",
